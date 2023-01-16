7a9d0f52d4d7e216e0fb43fd6697061425c85202
Léonard Krief <53581910+leonardkrief@users.noreply.github.com>
2023-01-17 00:52:40 +0100

    modif de strcmp et strncmp pour pas segfault sur des chaines vides

--- libft/srcs/perso/ft_strcmp.c
+++ libft/srcs/perso/ft_strcmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mgamil <mgamil@student.42.fr>              +#+  +:+       +#+        */
+/*   By: lkrief <lkrief@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 05:19:46 by mgamil            #+#    #+#             */
-/*   Updated: 2022/12/09 14:39:53 by mgamil           ###   ########.fr       */
+/*   Updated: 2023/01/16 21:02:36 by lkrief           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ int	ft_strcmp(const char *s1, const char *s2)
 	size_t	i;
 
 	i = 0;
-	while ((s1[i] || s2[i]))
+	while (s1[i] && s2[i])
 	{
 		if (s1[i] != s2[i])
 			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
-	return (0);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
