1e27a9ddf741630acf570fad8f2766244bd85932
Leonard KRIEF <lkrief@e2r5p16.clusters.42paris.fr>
2023-01-24 20:26:41 +0100

    builtins regles, manque les wildcards et expand

--- srcs/builtins/unset.c
+++ srcs/builtins/unset.c
@@ -6,7 +6,7 @@
 /*   By: lkrief <lkrief@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 17:47:54 by lkrief            #+#    #+#             */
-/*   Updated: 2023/01/17 00:44:07 by lkrief           ###   ########.fr       */
+/*   Updated: 2023/01/24 20:25:55 by lkrief           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_unset(char **tab, char ***addr_ev)
 	while (tab && *tab)
 	{
 		n = ft_ev_getvarindex(*tab, *addr_ev);
-		if (n >= 0)
+		if (n >= 0 && *addr_ev)
 			*addr_ev = regenerate_ev(ft_tablen(*addr_ev) - 1, n, *addr_ev);
 		tab++;
 	}
